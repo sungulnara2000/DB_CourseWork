a076913ce45c4efd4cf8bc1bd3724e125e53416d
sungatullina gulnara gabdullovna <sungulnara2000@mail.ru>
2019-05-21 12:54:04 +0300

    triggers

--- triggers.sql
+++ triggers.sql
@@ -1,5 +1,5 @@
-/* Если раюотник назначается на должность уже имея другую, то в обоих строках
- тип работы становится по совместительству*/
+/* Если работник назначается на должность уже имея другую, то в обоих строках
+ тип работы становится 1 (т. е. по совместительству) */
 create or replace function insert_employee() returns trigger as $insert_employee$
   begin
     update employee set type_of_work_cd = 1
@@ -13,11 +13,11 @@ create trigger insert_employee
     on employee
   for each row execute procedure insert_employee();
 
-/* При увольнении работника появляется вакасия на эту должность */
 
+/* При увольнении работника появляется вакасия на эту должность */
 create or replace function new_vacancy() returns trigger as $new_vacancy$
   begin
-    insert into vacancy values (10, old.org_unit_id, old.position_id, 1);
+    insert into vacancy values (DEFAULT, old.org_unit_id, old.position_id, 1);
     RETURN NULL;
   end;
 $new_vacancy$ language plpgsql;
@@ -27,6 +27,3 @@ create trigger delete_employee
     on employee
   for each row execute procedure new_vacancy();
 
-insert into employee values (100, 13, 1, 1, 0, 0, '2019-05-19');
-
-delete from employee where employee_id = 100
