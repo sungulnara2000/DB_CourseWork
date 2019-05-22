8bd8cca0af7e391c7f6a8c126ec839da3658f62d
sungatullina gulnara gabdullovna <sungulnara2000@mail.ru>
2019-05-22 19:03:47 +0300

    count salary

--- procedure.sql
+++ procedure.sql
@@ -7,15 +7,16 @@ AS $$
     begin
         SELECT INTO base salary_amt FROM (base_salary
           right join employee
-            on base_salary.position_id = employee.position_id)
+            on base_salary.position_id = employee.position_id
+                 and base_salary.org_unit_id = employee.org_unit_id)
         where employee_id = e_id;
         SELECT INTO al allowance_amt FROM (
                                            allowance
                                            right join employee_x_allowance
                                              on allowance.allowance_id = employee_x_allowance.allowance_id)
         where employee_id = e_id;
-        return ((base+al)/(working_days-holiday_days))*0.87;
+        return ((base+al)/working_days * holiday_days)*0.87;
 end;
 $$ LANGUAGE plpgsql;
 
-select count_paycheck(23, 20, 1);
+select count_paycheck(23, 20, 2);
