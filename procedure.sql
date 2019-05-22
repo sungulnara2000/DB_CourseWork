e8e0d877d1082447c6d52daabbc0fca80363a3bd
sungatullina gulnara gabdullovna <sungulnara2000@mail.ru>
2019-05-22 18:56:39 +0300

    procedure -> function

--- procedure.sql
+++ procedure.sql
@@ -1,22 +1,21 @@
-create table paycheck (
-  employee_id int, FOREIGN KEY (employee_id) REFERENCES employee(employee_id) ON DELETE CASCADE,
-  paychek INT
-);
-
-create procedure count_paycheck(working_days integer, holiday_days integer, e_id integer)
-LANGUAGE plpgsql
+create or replace function count_paycheck(working_days integer, holiday_days integer, e_id integer)
+returns int
 AS $$
   declare
     base INT := 0;
     al INT := 0;
     begin
-        SELECT INTO base salary_amt FROM base_salary where employee_id = e_id;
+        SELECT INTO base salary_amt FROM (base_salary
+          right join employee
+            on base_salary.position_id = employee.position_id)
+        where employee_id = e_id;
         SELECT INTO al allowance_amt FROM (
                                            allowance
                                            right join employee_x_allowance
-                                             on 1 = 1)
+                                             on allowance.allowance_id = employee_x_allowance.allowance_id)
         where employee_id = e_id;
-        insert into paycheck values (employee_id, ((base+al)/(working_days-holiday_days))*0.87);
-end $$;
+        return ((base+al)/(working_days-holiday_days))*0.87;
+end;
+$$ LANGUAGE plpgsql;
 
-CALL insert_data(23, 20, 1);
+select count_paycheck(23, 20, 1);
